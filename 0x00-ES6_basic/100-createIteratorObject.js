6148a296a9682741883a4bc242e17ef784ec0179
linetgitonga <linetgitonga55@gmail.com>
2024-07-29 17:12:10 +0000

    update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,4 @@
 export default function createIteratorObject(report) {
-
   return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
