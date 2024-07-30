5b77fe251c238dd2b9e89ce8bdde70d19df77f16
linetgitonga <linetgitonga55@gmail.com>
2024-07-30 17:36:11 +0000

    part3

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Set} set - The collection of unique items.
+ * @param {*} array - The array of items.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
